c6901af327bc2d64cf2eebefc1c0870dc5c7c85b
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-02-06 19:28:27 -0800

    Added ternary to declare the final score and winner

--- javascript.js
+++ javascript.js
@@ -4,46 +4,54 @@ const SCISSORS = 2;
 let player = 0;
 let computer = 0;
 
-const RPS = ["ROCK", "PAPER", "SCISSORS"];
-const computerRandom = Math.floor(Math.random() * RPS.length);
-
-let computerSelection = RPS[computerRandom]
-console.log(computerSelection)
-
-
-
-const playerPrompt = prompt("Rock, Paper, or Scissors?");
-let playerSelection = (playerPrompt.toUpperCase());
-console.log(playerSelection);
 
 
    
 
 for (let i = 0; i < 5; i++) {
 
+    const RPS = ["ROCK", "PAPER", "SCISSORS"];
+    const computerRandom = Math.floor(Math.random() * RPS.length);
+    
+    let computerSelection = RPS[computerRandom]
+    //console.log(computerSelection)
+    
+    
+    
+    const playerPrompt = prompt("Rock, Paper, or Scissors?");
+    let playerSelection = (playerPrompt.toUpperCase());
+    //console.log(playerSelection);
+    
+
+
+
+
+
         if ( (playerSelection === ("ROCK") ) && (computerSelection === ("SCISSORS")) ) {
-                alert("You won this round!");
+                alert("You won this round!") , player++;
             }       
             else if ( (playerSelection === "PAPER") && (computerSelection === "ROCK") ) {
-                alert ("You won this round!");
+                alert ("You won this round!"), player++ ;
             }   
             else if ( (playerSelection === "SCISSORS") && (computerSelection === "ROCK") ) {
-                alert ("The machine won this round!");
+                alert ("The machine won this round!") , computer++;
             }   
             else if ( (playerSelection === "ROCK") && (computerSelection === "PAPER") ) {
-                alert ("The machine won this round!");
+                alert ("The machine won this round!") , computer++;
             }   
             else if ( playerSelection > computerSelection) {
-                alert ("You won this round!")
+                alert ("You won this round!") , player++ ;
             }   
             else if (playerSelection === computerSelection){
                 alert ("You both are tied!");
             }   
             else{
-                alert ("The machine won this round!");
+                alert ("The machine won this round!") , computer++;
             }  
                                                 
         }
+
+let finalScore = (player > computer) ? alert("You have successfully defeated the machine!") : alert("The machine has defeated the player. GAME OVER");
 
 
 /* (if ( playerSelection > computerSelection) {
