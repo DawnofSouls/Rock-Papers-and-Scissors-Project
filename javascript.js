7e58db1abf654ce6a4fd094ac2fae6cd6be6214e
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-03-11 21:45:59 -0800

    Added "You lost" message if lost the game

--- javascript.js
+++ javascript.js
@@ -79,6 +79,13 @@ let ScreenDamageStyle = `
 let ScreenDamagePoint = () => {
   ScreenDamage.style = ScreenDamageStyle;
 };
+let BackgroundStyleText = `
+background-image: url(/assets/images/You_lost.svg);
+background-repeat: repeat;
+  background-position:0% 0%, 40px 40px;
+  background-size:80px;
+  min-height:100vh;
+`;
 
 let finalScore = () => {
   if (playerScore > computerScore) {
@@ -91,6 +98,7 @@ let finalScore = () => {
       bodyelement.removeChild(child);
       child = bodyelement.lastElementChild;
     }
+    document.querySelector("body").style = BackgroundStyleText;
   }
 };
 
